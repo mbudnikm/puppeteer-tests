9ae5cb303ac2c5bc631894aea6c88bbb6560ac74
mbudnikm <mbudnikm97@gmail.com>
2019-08-22 14:00:55 +0200

    finished po

--- test/po/todomvc.po.js
+++ test/po/todomvc.po.js
@@ -1,7 +1,25 @@
 const TodomvcPO = (page) => ({
+    selectors: {
+        newTodoInput: '.new-todo',
+        todoItems: '.todo-list .view',
+    },
+
     async addTodo(label) {
-        await page.click('.new-todo')
+        await page.click(this.selectors.newTodoInput)
         await page.keyboard.type(label)
         await page.keyboard.up('\n')
+    },
+
+    async visibleTodosCount() {
+        return page.$$eval(this.selectors.newTodoInput, todoItems => todoItems.length)
+    },
+
+    async todoWithLabelIsDisplayed(phrase) {
+        return page.$$eval(this.selectors.todoItems, 
+          (items, phrase) => Array.from(items).find(node => node.innerText.includes(phrase)), phrase)
     }
 })
+
+module.exports = {
+    TodomvcPO
+}
--- test/todomvc.spec.js
+++ test/todomvc.spec.js
@@ -1,23 +1,25 @@
 const puppeteer = require('puppeteer')
 const chalk = require('chalk')
 const assert = require('assert')
+const { TodomvcPO } = require('./po/todomvc.po.js')
 
 const { sleep } = require('./utils')
 const URL = 'http://todomvc.com/examples/jquery/'
 
 describe('todomvc application', () => {
-  let browser, page
+  let browser, page, po
 
   beforeAll(async () => {
     browser = await puppeteer.launch({
       headless: false,
-      executablePath: "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-      // slowMo: 100,
+      executablePath: "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe",
+      // slowMo: 50,
     })
   }, 100000);
 
   beforeEach(async () => {
     page = await browser.newPage()
+    po = TodomvcPO(page)
     await page.goto(URL, { waitUntil: 'networkidle2' })
   }, 100000)
 
@@ -35,19 +37,25 @@ describe('todomvc application', () => {
   for(let i=0; i < 3; i++) {
     it('should display a todo after it was added', async () => {
       // act
-      await page.click('.new-todo')
-      await page.keyboard.type('kup czekoladę <3')
-      await page.keyboard.up('\n')
+      // addTodo
+      // await page.click('.new-todo')
+      // await page.keyboard.type('kup czekoladę <3')
+      // await page.keyboard.up('\n')
+      let phrase = "kup czekoladę <3..."
+      await po.addTodo(phrase)
 
       //assert
-      let items
-      items = await page.$$eval('.todo-list .view', todoItems => todoItems.length)
+      // visibleTodosCoun
+      // let items
+      // items = await page.$$eval('.todo-list .view', todoItems => todoItems.length)
+      let items = await po.visibleTodosCount()
       expect(items).toEqual(1)
 
-      let phrase = "kup czekoladę <3"
-      await page.$$eval('.todo-list .view', 
-          (items, phrase) => Array.from(items).find(node => node.innerText.includes(phrase)), phrase)
-      
-    });
+      // todoWithLabelIsDisplayed
+      // await page.$$eval('.todo-list .view', 
+      //    (items, phrase) => Array.from(items).find(node => node.innerText.includes(phrase)), phrase)
+      let includes = await po.todoWithLabelIsDisplayed(phrase)
+      expect(includes).toBeTruthy()
+    }, 50000);
   }
 });
